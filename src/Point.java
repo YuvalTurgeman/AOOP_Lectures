2d52b598d6639792480d3864e7c5e3d9a8888c7e
Yuval <taltush2412@gmail.com>
2024-06-13 09:44:09 +0300

    Addition of Point class

---
+++ src/Point.java
@@ -0,0 +1,35 @@
+public class Point {
+    //fields
+    private double x;
+    private double y;
+
+    //ctors
+    public Point(){
+        x = 0;
+        y = 0;
+    }
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Object other){
+        if(other instanceof Point){
+            this.x = ((Point) other).x;
+            this.y = ((Point)other).y;
+        }
+    }
+
+    //getters+setters
+    public double getX(){return x;}
+
+    public double getY(){return y;}
+
+    //methods
+    public void move(double dx, double dy){
+        x += dx;
+        y += dy;
+    }
+
+}
